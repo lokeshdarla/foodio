b9b126fbfa3f8fa47ec9d395e4d93643da12df97
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-03-28 22:35:22 +0530

    [feat]: filtering options

--- server/src/controllers/food_items.ts
+++ server/src/controllers/food_items.ts
@@ -1,15 +1,50 @@
 import { Request, Response } from 'express';
-import { FoodItemModel } from '../models/schemas';
+import { FoodItemModel, CategoryModel, RestaurantModel } from '../models/schemas';
 import { IFoodItem } from '../models/schemas';
 
 export const getFoodItems = async (req: Request, res: Response): Promise<void> => {
   try {
-    const foodItems = await FoodItemModel.find();
-    res.status(200).json(foodItems);
+    const sortParam = req.query.sort;
+    const categoryParam = req.query.category;
+    const searchParam = req.query.search;
+
+    let sortCriteria: any = {};
+
+    if (sortParam === 'asc') {
+      sortCriteria = { price: 1 };
+    } else if (sortParam === 'desc') {
+      sortCriteria = { price: -1 };
+    }
+
+    let filterCriteria: any = {};
+    if (categoryParam) {
+      filterCriteria = { categoryId: categoryParam };
+    }
+
+    let searchCriteria: any = {};
+    if (searchParam) {
+      searchCriteria = { name: { $regex: searchParam, $options: 'i' } };
+    }
+
+    const foodItems: IFoodItem[] = await FoodItemModel.find({
+      ...filterCriteria,
+      ...searchCriteria
+    }).sort(sortCriteria);
+
+    const foodItemsWithDetails = await Promise.all(foodItems.map(async (foodItem) => {
+      const category = await CategoryModel.findById(foodItem.categoryId);
+      const restaurant = await RestaurantModel.findById(foodItem.restaurantId);
+      return { ...foodItem.toJSON(), category, restaurant };
+    }));
+
+    res.status(200).json(foodItemsWithDetails);
   } catch (error: any) {
     res.status(500).json({ message: 'Failed to get food items', error: error.message });
   }
 };
+
+
+
 
 export const createFoodItem = async (req: Request, res: Response): Promise<void> => {
   try {
